3bdb270b45a115accdb03f8081746a69b23015af
AbdulMalek-swe <abdulmalek.swe.585@gmail.com>
2023-06-17 10:17:54 +0600

    added any

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,8 +1,7 @@
 const jwt = require("jsonwebtoken");
 const {promisify} = require("util");
 module.exports.verifyToken = async(req,res,next) =>{
-    try{
-       
+    try{  
         const token = req.headers.authorization.split(" ")[1];
         if(!token){
             return res.status(401).json({
